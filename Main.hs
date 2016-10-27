9ec9cf30ac4ebc1ee0b7a2a61e2b495829390e4e
Dave Compton <davecompton7@gmail.com>
2016-10-27 22:47:16 +0000

    some name changes ; moving towards sayHello/getHello functions

--- Main.hs
+++ Main.hs
@@ -1,26 +1,32 @@
 import GHCJS.Marshal(fromJSVal)
 import GHCJS.Foreign.Callback (Callback, syncCallback1, OnBlocked(ContinueAsync))
 import Data.JSString (JSString, unpack, pack)
-import GHCJS.Types (JSVal)
+import GHCJS.Types (JSVal, jsval)
 
 getHello :: String -> String
 getHello name = "hello, " ++ name
+
+getHello' :: JSVal -> IO JSVal
+getHello' jv = do
+    Just str <- fromJSVal jv
+    let hello = getHello str
+    return (jsval . pack  $ hello)
 
 sayHello :: String -> IO ()
 sayHello name = print $ getHello name
 
 sayHello' :: JSVal -> IO ()
-sayHello' jsval = do
-    Just str <- fromJSVal jsval
+sayHello' jv = do
+    Just str <- fromJSVal jv
     sayHello $ unpack str
 
 foreign import javascript unsafe "js_sayHello = $1"
-    set_callback :: Callback a -> IO ()
+    set_sayHelloCallback :: Callback a -> IO ()
 
 foreign import javascript unsafe "js_sayHello($1)" 
     test_callback :: JSString -> IO ()
 
 main = do
-    callback <- syncCallback1 ContinueAsync sayHello'
-    set_callback callback
+    sayHelloCallback <- syncCallback1 ContinueAsync sayHello'
+    set_sayHelloCallback sayHelloCallback
     test_callback $ pack "world"
